5e8c7a7cc26fa296a7b5463649e0ee86b83bafdd
Nurullah Akkaya <nurullah@nakkaya.com>
2018-11-11 16:32:17 +0200

    set motor defaults

--- driver_o.h
+++ driver_o.h
@@ -19,32 +19,20 @@ class driver_o : public object {
     digitalWrite(CSPin, HIGH);
     digitalWrite(resetPin, LOW);
     digitalWrite(resetPin, HIGH);
-    // Before doing anything else, we need to
-    //  tell the objects which SPI port to use.
-    //  Some devices may have more than one.
-    board.SPIPortConnect(&SPI);
 
-    // BUSY pin low during operations;
-    // second paramter ignored.
+    board.SPIPortConnect(&SPI);
     board.configSyncPin(BUSY_PIN, 0);
-    board.configStepMode(STEP_FS);             // 0 microsteps per step
+    board.configStepMode(STEP_FS);
     board.setMinSpeed(0);
-    board.setMaxSpeed(200);                   // 200 steps/s max
+    board.setMaxSpeed(200);
     board.setFullSpeed(0x027);
     board.setAcc(0xFFF);
     board.setDec(0xFFF);
-    board.setSlewRate(SR_530V_us);             // Upping the edge speed increases torque.
-    board.setOCThreshold(OC_6000mA);            // OC threshold 750mA
-    board.setPWMFreq(PWM_DIV_2, PWM_MUL_2);    // 31.25kHz PWM freq
-    board.setOCShutdown(OC_SD_DISABLE);        // don't shutdown on OC
-    board.setVoltageComp(VS_COMP_DISABLE);     // don't compensate for motor V
-    board.setSwitchMode(SW_USER);              // Switch is not hard stop
-    board.setOscMode(INT_16MHZ_OSCOUT_16MHZ);  // for board, we want 16MHz
-    // We'll tinker with these later, if needed.
-    board.setAccKVAL(255);
-    board.setDecKVAL(255);
-    board.setRunKVAL(255);
-    // This controls the holding current; keep it low.
+    board.setOCThreshold(OC_6000mA);
+    board.setOscMode(INT_16MHZ_OSCOUT_16MHZ);
+    board.setAccKVAL(254);
+    board.setDecKVAL(254);
+    board.setRunKVAL(254);
     board.setHoldKVAL(32);
   }
 
